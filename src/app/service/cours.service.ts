65580360e25908defffcab1fc2b0d5009031adae
Chris Marie-Angélique <christopher2211.angelique@gmail.com>
2023-10-23 01:14:37 +0200

    add files

--- src/app/service/cours.service.ts
+++ src/app/service/cours.service.ts
@@ -7,8 +7,8 @@ import { Observable } from 'rxjs/internal/Observable';
 })
 export class CoursService {
 
-  // private apiUrl  = 'http://localhost:3000/cours';
-  private apiUrl  = 'https://ap-js-vacataires-api.vercel.app/cours';
+  private apiUrl  = 'http://localhost:3000/cours';
+  // private apiUrl  = 'https://ap-js-vacataires-api.vercel.app/cours';
 
   constructor(private http: HttpClient) { }
 
